4b2305e30c48df211ee0378f005b86c8be34b09b
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-10-17 21:53:39 +0700

    up

--- ES6/exercises_extra_3.js
+++ ES6/exercises_extra_3.js
@@ -1,18 +1,20 @@
-let getInfo = (student) =>{
-    const { firstName = "Quân", degree = "NA" } = student;
+let getInfo = ({ firstName = "Quân", degree = "NA" }) => {
     console.log(`First Name: ${firstName}`);
     console.log(`Degree: ${degree}`);
-}
+};
+
 const student1 = {
     firstName: 'John',
     degree: 'Bachelor',
     gender: 'male',
     english: "English"
-}
+};
+
 const student2 = {
     name: 'John',
     gender: 'male',
     english: "English"
-}
+};
+
 getInfo(student1);
 getInfo(student2);
